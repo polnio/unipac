836c1c44d5b4c22a20306fc867011128d28ab475
Po Co <polniolino@gmail.com>
2024-10-01 16:03:16 +0200

    Use global packages for plugin example

--- crates/plugins/dummy/src/lib.rs
+++ crates/plugins/dummy/src/lib.rs
@@ -1,5 +1,7 @@
+static INSTALLED_PACKAGES: [&str; 1] = ["dummy-rs"];
+
 fn list_packages() -> Result<Vec<String>, String> {
-    Ok(vec!["dummy".into()])
+    Ok(INSTALLED_PACKAGES.into_iter().map(String::from).collect())
 }
 
 unipac_shared::export_plugin!();
