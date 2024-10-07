87c9096f6bca55b437c60ce2e75e57c0fbff1d43
Po Co <polniolino@gmail.com>
2024-10-07 22:13:34 +0200

    Fix c example

--- examples/example-plugin-c/lib.c
+++ examples/example-plugin-c/lib.c
@@ -2,16 +2,20 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define N_INSTALLED_PLUGIN 1
-static char *INSTALLED_PLUGIN[N_INSTALLED_PLUGIN] = {"example-c"};
+#define N_INSTALLED_PLUGINS 1
+static char *INSTALLED_PLUGINS[N_INSTALLED_PLUGINS] = {"example-c"};
 
 ListPackagesResult_t ffi_list_packages() {
-  char **installed_plugin = malloc(sizeof(char *) * N_INSTALLED_PLUGIN);
-  installed_plugin[0] = malloc(sizeof(char) * 15);
-  /* installed_plugin[0] = "example-malloc"; */
-  memcpy(installed_plugin[0], "example-malloc", 15);
-  slice_boxed_char_ptr_t data = {.ptr = installed_plugin,
-                                 .len = N_INSTALLED_PLUGIN};
+  char **installed_plugins = malloc(sizeof(char *) * N_INSTALLED_PLUGINS);
+  for (size_t i = 0; i < N_INSTALLED_PLUGINS; i++) {
+    char *installed_plugin = INSTALLED_PLUGINS[i];
+    size_t len = strlen(installed_plugin);
+    installed_plugins[i] = malloc(sizeof(char) * len);
+    memcpy(installed_plugins[i], installed_plugin, len);
+  }
+
+  slice_boxed_char_ptr_t data = {.ptr = installed_plugins,
+                                 .len = N_INSTALLED_PLUGINS};
   ListPackagesResult_t result = {.err = NULL, .data = data};
   return result;
 }
