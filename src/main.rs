40c3b0754e7edba9467c228d7ff2776a98a3adc6
Po Co <polniolino@gmail.com>
2025-02-09 17:19:49 +0100

    Use async

--- src/main.rs
+++ src/main.rs
@@ -14,8 +14,35 @@ pub use plugin::Plugin;
 use anyhow::{Context as _, Result};
 use clap::Parser as _;
 use spinners::Spinners;
+use std::future::Future;
+use tokio::runtime::Handle;
+use tokio::task::JoinSet;
 
-fn main() -> Result<()> {
+trait IteratorExt<T> {
+    fn to_set(self) -> JoinSet<T>;
+}
+impl<I, F, T> IteratorExt<T> for I
+where
+    T: Send + 'static,
+    F: Future<Output = T> + Send + 'static,
+    I: Iterator<Item = F> + Sized,
+{
+    fn to_set(self) -> JoinSet<T> {
+        JoinSet::from_iter(self)
+    }
+}
+
+trait FutureExt<T> {
+    fn await_blocking(self) -> T;
+}
+impl<T, F: Future<Output = T> + Send> FutureExt<T> for F {
+    fn await_blocking(self) -> T {
+        tokio::task::block_in_place(|| Handle::current().block_on(self))
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
     let args = Args::parse();
     let config = Config::from_opt_file(args.config_path)?;
     let spinners = Spinners::new();
@@ -27,24 +54,51 @@ fn main() -> Result<()> {
         .map(|plugin| {
             // DEBUG: Simulate different speeds
             // std::thread::sleep(std::time::Duration::from_secs(2));
-            let (progress_sender, progress_receiver) = std::sync::mpsc::channel();
-            let plugin = Plugin::new(plugin, progress_sender);
-            let id = plugin.get_id().context("Failed to get id")?;
-            let name = plugin.get_name().context("Failed to get name")?;
+            let (progress_sender, mut progress_receiver) = tokio::sync::mpsc::channel(100);
+            let (end_sender, mut end_receiver) = tokio::sync::mpsc::channel(100);
+            let plugin = Plugin::new(plugin, progress_sender, end_sender);
+            let id = plugin
+                .get_id()
+                .await_blocking()
+                .context("Failed to get id")?;
+            let name = plugin
+                .get_name()
+                .await_blocking()
+                .context("Failed to get name")?;
             let spinner = spinners.add(name.clone());
-            let pbh = std::thread::spawn(move || {
-                while let Ok(progress) = progress_receiver.recv() {
-                    spinner.set(progress);
-                    if progress == 100 {
-                        spinner.finish();
-                        break;
+            spinner.set(0);
+            let spinners = spinners.clone();
+            anyhow::Ok(async move {
+                let pbh = async {
+                    loop {
+                        tokio::select! {
+                            _ = end_receiver.recv() => break,
+                            Some(progress) = progress_receiver.recv() => {
+                                spinner.set(progress);
+                                if progress == 100 {
+                                    spinner.finish();
+                                    break;
+                                }
+                            }
+                        };
+                    }
+                };
+                let ph = async {
+                    plugin
+                        .list_packages()
+                        .await
+                        .context("Failed to list packages")
+                };
+                let (_, packages) = tokio::join!(pbh, ph);
+                match packages {
+                    Ok(packages) => Some((id, name, packages)),
+                    Err(err) => {
+                        spinners.clear().unwrap();
+                        eprintln!("[{}] Error: {:#}", name, err);
+                        None
                     }
                 }
-            });
-            let ph = std::thread::spawn(move || {
-                plugin.list_packages().context("Failed to list packages")
-            });
-            anyhow::Ok((id, name, pbh, ph))
+            })
         })
         .filter_map(|r| match r {
             Ok(r) => Some(r),
@@ -54,20 +108,11 @@ fn main() -> Result<()> {
                 None
             }
         })
-        .collect::<Vec<_>>()
+        .to_set()
+        .join_all()
+        .await
         .into_iter()
-        .filter_map(|(id, name, pbh, ph)| {
-            let packages = ph.join().unwrap();
-            pbh.join().unwrap();
-            match packages {
-                Ok(packages) => Some((id, name, packages)),
-                Err(err) => {
-                    spinners.clear().unwrap();
-                    eprintln!("[{}] Error: {:#}", name, err);
-                    None
-                }
-            }
-        })
+        .filter_map(|r| r)
         .collect::<Vec<_>>();
 
     spinners.clear().unwrap();
--- src/plugin.rs
+++ src/plugin.rs
@@ -1,15 +1,15 @@
 use crate::{Package, Result};
 use anyhow::{bail, Context as _};
-use std::io::{BufRead as _, BufReader};
-use std::process::Command;
-use std::sync::mpsc;
-use std::thread::JoinHandle;
+use tokio::io::{AsyncBufReadExt as _, BufReader};
+use tokio::process::Command;
+use tokio::sync::mpsc;
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 enum Response {
     Progress(u8),
     Response(String),
     Package(Package),
+    End,
     Error(String),
 }
 impl Response {
@@ -33,125 +33,152 @@ impl From<Result<Package>> for Response {
 pub struct Plugin {
     pub path: String,
     response_sender: mpsc::Sender<Response>,
-    response_receiver: mpsc::Receiver<Response>,
+    // response_receiver: mpsc::Receiver<Response>,
+    response_receiver: tokio::sync::Mutex<mpsc::Receiver<Response>>,
     progress_sender: mpsc::Sender<u8>,
+    end_sender: mpsc::Sender<()>,
 }
 
 macro_rules! impl_plugin_inner {
-    ($vis:vis fn $name:ident ($($field:ident : $type:ty),*) -> $return:ty, $getter:ident) => {
+    ($vis:vis async fn $name:ident ($($field:ident : $type:ty),*) -> $return:ty, $getter:ident) => {
         impl Plugin {
-            $vis fn $name(&self, $($field: $type),*) -> Result<$return> {
+            $vis async fn $name(&self, $($field: $type),*) -> Result<$return> {
                 let handle = self.start_subcommand(stringify!($name), vec![$($field),*]);
-                let packages = self.$getter()?;
-                let _ = handle.join();
+                let packages = self.$getter();
+                let (_, packages) = tokio::try_join!(handle, packages)?;
                 Ok(packages)
             }
         }
     };
 }
 macro_rules! impl_plugin {
-    ($vis:vis fn $name:ident ($($field:ident : $type:ty),*) -> Vec<Package>) => {
-        impl_plugin_inner!($vis fn $name ($($field : $type),*) -> Vec<Package>, get_packages);
+    ($vis:vis async fn $name:ident ($($field:ident : $type:ty),*) -> Vec<Package>) => {
+        impl_plugin_inner!($vis async fn $name ($($field : $type),*) -> Vec<Package>, get_packages);
     };
-    ($vis:vis fn $name:ident ($($field:ident : $type:ty),*) -> String) => {
-        impl_plugin_inner!($vis fn $name ($($field : $type),*) -> String, get_response);
+    ($vis:vis async fn $name:ident ($($field:ident : $type:ty),*) -> String) => {
+        impl_plugin_inner!($vis async fn $name ($($field : $type),*) -> String, get_response);
     };
 }
 
-impl_plugin!(pub fn get_id() -> String);
-impl_plugin!(pub fn get_name() -> String);
-impl_plugin!(pub fn list_packages() -> Vec<Package>);
+impl_plugin!(pub async fn get_id() -> String);
+impl_plugin!(pub async fn get_name() -> String);
+impl_plugin!(pub async fn list_packages() -> Vec<Package>);
 impl Plugin {
-    pub fn new(path: String, progress_sender: mpsc::Sender<u8>) -> Self {
-        let (response_sender, response_receiver) = mpsc::channel();
+    pub fn new(
+        path: String,
+        progress_sender: mpsc::Sender<u8>,
+        end_sender: mpsc::Sender<()>,
+    ) -> Self {
+        let (response_sender, response_receiver) = mpsc::channel(100);
         Self {
             path,
             response_sender,
-            response_receiver,
+            response_receiver: response_receiver.into(),
             progress_sender,
+            end_sender,
         }
     }
-    fn get_response(&self) -> Result<String> {
+    async fn get_response(&self) -> Result<String> {
+        let mut response_receiver = self.response_receiver.lock().await;
         loop {
-            let response = self.response_receiver.recv().unwrap();
+            let response = response_receiver.recv().await.unwrap();
             match response {
                 Response::Response(response) => return Ok(response),
-                Response::Progress(progress) => self.progress_sender.send(progress).unwrap(),
-
-                Response::Error(error) => bail!(error),
-                _ => continue,
+                Response::Progress(progress) => self.progress_sender.send(progress).await.unwrap(),
+                Response::End => {
+                    self.end_sender.send(()).await.unwrap();
+                    bail!("Ended without response")
+                }
+                Response::Error(error) => {
+                    self.end_sender.send(()).await.unwrap();
+                    bail!(error)
+                }
+                _ => {}
             }
         }
     }
-    fn get_packages(&self) -> Result<Vec<Package>> {
+    async fn get_packages(&self) -> Result<Vec<Package>> {
         let mut packages = Vec::new();
+        let mut response_receiver = self.response_receiver.lock().await;
         loop {
-            match self.response_receiver.recv().unwrap() {
+            match response_receiver.recv().await.unwrap() {
                 Response::Package(package) => packages.push(package),
                 Response::Progress(progress) => {
-                    self.progress_sender.send(progress).unwrap();
+                    self.progress_sender.send(progress).await.unwrap();
                     if progress == 100 {
                         break;
                     }
                 }
-                Response::Error(error) => bail!(error),
-                _ => continue,
+                Response::End => {
+                    self.end_sender.send(()).await.unwrap();
+                    break;
+                }
+                Response::Error(error) => {
+                    self.end_sender.send(()).await.unwrap();
+                    bail!(error)
+                }
+                _ => {}
             }
         }
         Ok(packages)
     }
-    fn start_subcommand(&self, command: &'static str, args: Vec<String>) -> JoinHandle<Result<()>> {
+    async fn start_subcommand(&self, command: &'static str, args: Vec<String>) -> Result<()> {
         // while self.response_receiver.try_recv().is_ok() {}
         let response_sender = self.response_sender.clone();
         let path = self.path.clone();
-        std::thread::spawn(move || {
-            let mut cmd = Command::new("bash")
-                .arg("-c")
-                .arg(format!("{} {} {}", path, command, args.join(" ")))
-                .stdout(std::process::Stdio::piped())
-                .spawn()
-                .context("Failed to run plugin")?;
-            let stdout = cmd.stdout.as_mut().context("Failed to get stdout")?;
-            let lines = BufReader::new(stdout).lines();
-            let mut fields: Option<Vec<String>> = None;
-            for line in lines {
-                // let line = line.expect("Failed to read line");
-                let Ok(line) = line else {
-                    continue;
-                };
-                let Some((channel, data_str)) = line.split_once(' ') else {
-                    continue;
-                };
-                match channel {
-                    "Package" => {
-                        // let _ = response_sender.send(Response::from(Package::from_json(data_str)));
-                        if let Some(fields) = &fields {
-                            let data = fields
-                                .iter()
-                                .zip(data_str.split(','))
-                                .map(|(field, value)| (field.to_owned(), value.to_owned()))
-                                .collect();
-                            let package = Package::new(data);
-                            let _ = response_sender.send(Response::from(Ok(package)));
-                        } else {
-                            fields = Some(data_str.split(',').map(str::to_owned).collect());
-                        }
+        let mut cmd = Command::new("bash")
+            .arg("-c")
+            .arg(format!("{} {} {}", path, command, args.join(" ")))
+            .stdout(std::process::Stdio::piped())
+            .spawn()
+            .context("Failed to run plugin")?;
+        let stdout = cmd.stdout.as_mut().context("Failed to get stdout")?;
+        let mut lines = BufReader::new(stdout).lines();
+        let mut fields: Option<Vec<String>> = None;
+        while let Some(line) = lines.next_line().await? {
+            // let line = line.expect("Failed to read line");
+            let Some((channel, data_str)) = line.split_once(' ') else {
+                continue;
+            };
+            match channel {
+                "Package" => {
+                    // let _ = response_sender.send(Response::from(Package::from_json(data_str)));
+                    if let Some(fields) = &fields {
+                        let data = fields
+                            .iter()
+                            .zip(data_str.split(','))
+                            .map(|(field, value)| (field.to_owned(), value.to_owned()))
+                            .collect();
+                        let package = Package::new(data);
+                        let _ = response_sender.send(Response::from(Ok(package))).await;
+                    } else {
+                        fields = Some(data_str.split(',').map(str::to_owned).collect());
                     }
-                    "Progress" => {
-                        let _ = response_sender.send(Response::new_progress_from_str(data_str));
-                    }
-                    "Response" => {
-                        let _ = response_sender.send(Response::Response(data_str.to_owned()));
-                    }
-                    "Error" => {
-                        let _ = response_sender.send(Response::Error(data_str.to_owned()));
-                    }
-                    _ => continue,
                 }
+                "Progress" => {
+                    let _ = response_sender
+                        .send(Response::new_progress_from_str(data_str))
+                        .await;
+                }
+                "Response" => {
+                    let _ = response_sender
+                        .send(Response::Response(data_str.to_owned()))
+                        .await;
+                }
+                "End" => {
+                    let _ = response_sender.send(Response::End).await;
+                    break;
+                }
+                "Error" => {
+                    let _ = response_sender
+                        .send(Response::Error(data_str.to_owned()))
+                        .await;
+                }
+                _ => continue,
             }
+        }
 
-            cmd.wait().expect("Failed to wait for plugin");
-            Ok(())
-        })
+        cmd.wait().await.expect("Failed to wait for plugin");
+        Ok(())
     }
 }
