4af203f4f49dfdd395b11420a8f04703fee551a6
Po Co <polniolino@gmail.com>
2025-04-16 18:59:39 +0100

    Add unipac-shared to packages

--- flake.nix
+++ flake.nix
@@ -43,25 +43,54 @@
             src = craneLib.cleanCargoSource ./.;
             strictDeps = true;
           };
+          unipac-shared = buildBash {
+            name = "unipac-shared";
+            src = ./unipac-shared;
+            buildPhase = "cp * $out/bin";
+          };
           plugins = [ "pacman" ];
+
+          buildBash =
+            {
+              name,
+              src,
+              buildPhase,
+              args ? { },
+            }:
+            pkgs.stdenvNoCC.mkDerivation (
+              {
+                pname = name;
+                inherit src;
+                version = unipac.version;
+                buildPhase = ''
+                  mkdir -p $out/bin
+                  ${buildPhase}
+                '';
+                strictDeps = true;
+                buildInputs = with pkgs; [ bash ];
+              }
+              // args
+            );
 
           mkPlugin =
             name:
-            pkgs.stdenvNoCC.mkDerivation {
-              pname = "unipac-plugin-${name}";
-              version = unipac.version;
+            buildBash {
+              name = "unipac-plugin-${name}";
               src = ./plugins/${name};
-              buildPhase = ''
-                mkdir -p $out/bin
-                cp -r ${name}.sh $out/bin/unipac-plugin-${name}
-              '';
-              strictDeps = true;
-              buildInputs = with pkgs; [ bash ];
+              buildPhase = "cp -r ${name}.sh $out/bin/unipac-plugin-${name}";
+              args = {
+                nativeBuildInputs = [ pkgs.makeWrapper ];
+                installPhase = ''
+                  mkdir -p $out/bin
+                  wrapProgram $out/bin/unipac-plugin-${name} \
+                    --prefix PATH : ${unipac-shared}/bin
+                '';
+              };
             };
           pluginsPkgs = lib.genAttrs plugins mkPlugin;
         in
         {
-          inherit unipac;
+          inherit unipac unipac-shared;
           default = unipac;
           unipac-plugin-pacman = mkPlugin "pacman";
         }
